e2e886ece2177456ce705e05a0ccee6eb0bceb02
David Tremmel <tremmel.david@hotmail.com>
2022-12-12 21:56:39 +0100

    create Spring backend

---
+++ movie-picker-be/src/main/java/hu/curiouswizard/moviepicker/MoviepickerApplication.java
@@ -0,0 +1,13 @@
+package hu.curiouswizard.moviepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepickerApplication.class, args);
+	}
+
+}
---
+++ movie-picker-be/src/test/java/hu/curiouswizard/moviepicker/MoviepickerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.curiouswizard.moviepicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviepickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
